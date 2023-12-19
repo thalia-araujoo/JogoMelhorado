50724eea30e4b02625d2587afd7f113fb96d41d2
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-12-19 19:37:29 -0300

    Update Lufy.java

--- Lufy.java
+++ Lufy.java
@@ -1,11 +1,7 @@
+
+// Classe Lufy (subclasse de Personagem)
 class Lufy extends Personagem {
     public Lufy(String nome, int vida, Arma arma, Poder poder) {
         super(nome, vida, arma, poder);
-    }
-    @Override
-    public void usarPoderLufy(Personagem oponente) {
-        // Lógica específica para o poder de Lufy
-        System.out.println("Lufy usa o poder de Endurecimento!");
-        oponente.sofrerDano(this.getPoder().getDano());
     }
 }
