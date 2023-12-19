565243ed4208e719b3eff0a3b87886e30cab92b7
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-12-19 19:36:42 -0300

    Update Jogo.java

--- Jogo.java
+++ Jogo.java
@@ -1,3 +1,5 @@
+
+
 import java.util.Scanner;
 
 // Classe Jogo
@@ -7,34 +9,32 @@ public class Jogo {
 
         // Introdução do Jogo
         System.out.println("##############################################################################\n");
-        System.out.println("\tBem-vindos ao Jogo de Luta entre Piratas!");
-        System.out.println("\tZoro, o maior espadachim do mundo e \n \tKisame, o melhor espadachim da nevoa,\n \tLufy o rei dos piratas.");
+        System.out.println("\tBem-vindos ao Jogo de Luta de One Piece!");
+        System.out.println("\tZoro, o maior espadachim do mundo.  \n \tKisame, o melhor espadachim da névoa,\n \te Lufy o rei dos piratas.");
         System.out.println("\tCada personagem tem sua arma única e poder especial.");
         System.out.println("\tEscolha sabiamente suas ações durante a batalha para sair vitorioso!\n");
         System.out.println("##############################################################################\n");
 
         // Criação de instâncias de armas
-        Arma espadaEnma = new Arma("Espada Enma", 500, 80, null);
-        Arma samehada = new Arma("Samehada", 300, 60, null);
-        Arma balaDBorracha = new Arma("Bala De Borracha", 700, 100, "Endurecimento");
+        Arma espadaEnma = new Arma("Espada Enma", 40, 47, "Envenenamento");
+        Arma samehada = new Arma("Samehada", 45, 60, "Congelamento");
+        Arma Haki = new Arma("Haki", 55, 40, "Fogo");
 
         // Criação de instâncias de poderes
-        Poder envenenamento = new Poder("Envenenamento", 20, 40);
-        Poder congelamento = new Poder("Congelamento", 30, 50);
-        Poder endurecimento = new Poder("Endurecimento", 70, 90);
+        Poder envenenamento = new PoderEnvenenamento();
+        Poder congelamento = new PoderCongelamento();
+        Poder Fogo = new Poder_Fogo();
 
         // Criação de instâncias de personagens
         Zoro zoro = new Zoro("Zoro", 100, espadaEnma, envenenamento);
         Kisame kisame = new Kisame("Kisame", 100, samehada, congelamento);
-        Lufy lufy = new Lufy("Lufy", 100, balaDBorracha, endurecimento);
+        Lufy lufy = new Lufy("Lufy", 100, Haki, Fogo);
 
         // Escolha do Personagem
         System.out.println("Escolha com qual personagem você deseja jogar:");
         System.out.println("1. Zoro");
         System.out.println("2. Kisame");
         System.out.println("3. Lufy");
-        
-
 
         int escolhaPersonagem = scanner.nextInt();
         Personagem jogadorEscolhido = null;
@@ -56,8 +56,10 @@ public class Jogo {
 
         // Exibir status inicial do personagem escolhido
         exibirStatusPersonagem(jogadorEscolhido);
+        
 
-        // Iniciar batalha chamando o metodo iniciar batalha
+
+        // Iniciar batalha chamando o método iniciar batalha
         Batalha batalha = new Batalha();
         batalha.iniciarBatalha(jogadorEscolhido, scanner);
 
@@ -73,8 +75,6 @@ public class Jogo {
         }
         System.out.println("-----------------------------");
     }
-/*Em resumo, a classe Jogo coordena a introdução do jogo,
- a criação dos elementos do jogo (armas, poderes, personagens)
- e inicia a batalha com o personagem escolhido pelo jogador. */
-   
+
+    
 }
