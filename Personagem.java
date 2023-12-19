8eb5637f7ec83233d3d0b2c8e866e652b0dd0e76
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-12-19 19:37:51 -0300

    Update Personagem.java

--- Personagem.java
+++ Personagem.java
@@ -1,16 +1,20 @@
 
+// Classe base para Personagem
 class Personagem {
+    //Encapsulamento
     private String nome;
     private int vida;
     private Arma arma;
     private Poder poder;
     private int mana;
 
+    //construtor
     public Personagem(String nome, int vida, Arma arma, Poder poder) {
         this.nome = nome;
         this.vida = vida;
         this.arma = arma;
         this.poder = poder;
+        this.mana = 100; // Valor inicial da mana
     }
 
     public String getNome() {
@@ -28,25 +32,25 @@ class Personagem {
     public Poder getPoder() {
         return poder;
     }
-
+    //metodo para receber dano
     public void sofrerDano(int dano) {
         this.vida -= dano;
         if (vida < 0) {
             vida = 0;
         }
     }
-
+    //ele vai retornar true ou false para se tem mana suficiente
     public boolean temManaSuficiente(int custoMana) {
         return this.mana >= custoMana;
     }
-    
+    //ele diminui a mana atual
     public void gastarMana(int custoMana) {
         this.mana -= custoMana;
     }
-
-/*Verifica se o personagem possui um poder especial.
-Se sim, utiliza o poder chamando o método usarPoder da instância de Poder. */
-    public String usarPoder(Personagem oponente) {
+    
+    /*Verifica se o personagem possui um poder especial.
+    Se sim, utiliza o poder chamando o método usarPoder da instância de Poder. */
+    public void usarPoder(Personagem oponente) {
         if (this.getPoder() != null) {
             String nomePoder = this.getPoder().usarPoder(this, oponente);
             if (nomePoder != null) {
@@ -55,24 +59,5 @@ Se sim, utiliza o poder chamando o método usarPoder da instância de Poder. */
         } else {
             System.out.println("Este personagem não possui um poder associado.");
         }
-        return nomePoder;
-    }
-
-    public void usarPoderZoro(Personagem oponente) {
-
-        System.out.println("Zoro usa o poder!");
-        oponente.sofrerDano(this.getPoder().getDano());
-    }
-
-    public void usarPoderKisame(Personagem oponente) {
-        
-        System.out.println("Kisame usa o poder!");
-        oponente.sofrerDano(this.getPoder().getDano());
-    }
-
-    public void usarPoderLufy(Personagem oponente) {
-        
-        System.out.println("Lufy usa o poder!");
-        oponente.sofrerDano(this.getPoder().getDano());
     }
 }
