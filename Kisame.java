5bd762c30e3c392b9e7755d577255ead61903a0b
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-12-19 19:37:05 -0300

    Update Kisame.java

--- Kisame.java
+++ Kisame.java
@@ -1,12 +1,7 @@
 
+// Classe Kisame (subclasse de Personagem)
 class Kisame extends Personagem {
     public Kisame(String nome, int vida, Arma arma, Poder poder) {
         super(nome, vida, arma, poder);
-    }
-    @Override
-    public void usarPoderKisame(Personagem oponente) {
-        // Lógica específica para o poder de Kisame
-        System.out.println("Kisame usa o poder de congelamento!");
-        oponente.sofrerDano(this.getPoder().getDano());
     }
 }
